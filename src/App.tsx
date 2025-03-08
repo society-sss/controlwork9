e32bd260286ac65508b7c707e8f983d7287faa7c
JalalovElzar <qwwgcc491@gmail.com>
2025-03-08 15:47:19 +0600

    добавил навигацию на кнопку rename при нажатии на которую переходим в форму для изменении

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header/Header'
-import Total from './components/Total/Total'
 import TransactionList from './components/TransactionList/TransactionList'
 import MainWindow from './containers/MainWindow/MainWindow'
 import FormTransaction from './containers/FormTransaction/FormTransaction'
@@ -15,6 +13,7 @@ function App() {
         <Route path='/' element={<MainWindow/>}>
           <Route index element={<TransactionList/>}/>
           <Route path='add-transaction' element={<FormTransaction/>}/>
+          <Route path='rename-transaction/:id' element={<FormTransaction/>}/>
         </Route>
       </Routes>
     </>
--- src/components/TransactionList/TransactionList.tsx
+++ src/components/TransactionList/TransactionList.tsx
@@ -4,10 +4,13 @@ import Spinner from '../../UI/Spinner/Spinner';
 import Total from '../Total/Total';
 import './transactionList.css'
 import { deleteTransaction, getTransaction } from '../../store/transaction/transactionThunks';
+import { useNavigate } from 'react-router-dom';
 
 const TransactionList = () => {
     const {loading, transactions} = useAppSelector(state => state.transaction)
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
     useEffect(() => {
         dispatch(getTransaction())
     }, [dispatch])
@@ -30,7 +33,7 @@ const TransactionList = () => {
                         </div>
                         <div className="transaction-delete-or-rename">
                             <p>{tr.type === 'income' ? `+${tr.amount}` : `-${tr.amount}`} kgs</p>
-                            <button type="button" className="btn btn-warning">rename</button>
+                            <button type="button" className="btn btn-warning" onClick={() => navigate(`rename-transaction/${tr.id}`)}>rename</button>
                             <button type="button" className="btn btn-danger" onClick={() => deleteTransactionFunction(tr.id)}>delete</button>
                         </div>
                     </div>
